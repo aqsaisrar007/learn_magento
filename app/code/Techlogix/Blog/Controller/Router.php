0b12739480eb9fb0c7da27216242e7bfbccf4709
Aqsa Israr <Aqsa Israr>
2016-04-08 15:21:44 +0500

    adminhtml successfully working

--- app/code/Techlogix/Blog/Controller/Router.php
+++ app/code/Techlogix/Blog/Controller/Router.php
@@ -1,53 +1,84 @@
 <?php
 namespace Techlogix\Blog\Controller;
-
+/**
+ * Cms Controller Router
+ *
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
 class Router implements \Magento\Framework\App\RouterInterface
 {
     /**
      * @var \Magento\Framework\App\ActionFactory
      */
     protected $actionFactory;
-
+    /**
+     * Event manager
+     *
+     * @var \Magento\Framework\Event\ManagerInterface
+     */
+    protected $_eventManager;
     /**
      * Post factory
      *
      * @var \Techlogix\Blog\Model\PostFactory
      */
     protected $_postFactory;
-
+    /**
+     * Config primary
+     *
+     * @var \Magento\Framework\App\State
+     */
+    protected $_appState;
+    /**
+     * Url
+     *
+     * @var \Magento\Framework\UrlInterface
+     */
+    protected $_url;
+    /**
+     * Response
+     *
+     * @var \Magento\Framework\App\ResponseInterface
+     */
+    protected $_response;
     /**
      * @param \Magento\Framework\App\ActionFactory $actionFactory
+     * @param \Magento\Framework\Event\ManagerInterface $eventManager
+     * @param \Magento\Framework\UrlInterface $url
      * @param \Techlogix\Blog\Model\PostFactory $postFactory
+     * @param \Magento\Framework\App\ResponseInterface $response
      */
     public function __construct(
         \Magento\Framework\App\ActionFactory $actionFactory,
-        \Techlogix\Blog\Model\PostFactory $postFactory
+        \Magento\Framework\Event\ManagerInterface $eventManager,
+        \Magento\Framework\UrlInterface $url,
+        \Techlogix\Blog\Model\PostFactory $postFactory,
+        \Magento\Framework\App\ResponseInterface $response
     ) {
         $this->actionFactory = $actionFactory;
-        $this->_postFactory = $postFactory;
+        $this->_eventManager = $eventManager;
+        $this->_url = $url;
+        $this->_pageFactory = $postFactory;
+        $this->_response = $response;
     }
-
     /**
-     * Validate and Match Blog Post and modify request
+     * Validate and Match Cms Page and modify request
      *
      * @param \Magento\Framework\App\RequestInterface $request
      * @return bool
      */
-    public function match(\Magento\Framework\App\RequestInterface $request) //required for RouterInterface
+    public function match(\Magento\Framework\App\RequestInterface $request)
     {
         $url_key = trim($request->getPathInfo(), '/blog/');
         $url_key = rtrim($url_key, '/');
-
         /** @var \Techlogix\Blog\Model\Post $post */
-        $post = $this->_postFactory->create();
+        $post = $this->_pageFactory->create();
         $post_id = $post->checkUrlKey($url_key);
         if (!$post_id) {
             return null;
         }
-
         $request->setModuleName('blog')->setControllerName('view')->setActionName('index')->setParam('post_id', $post_id);
         $request->setAlias(\Magento\Framework\Url::REWRITE_REQUEST_PATH_ALIAS, $url_key);
-
         return $this->actionFactory->create('Magento\Framework\App\Action\Forward');
     }
 }
