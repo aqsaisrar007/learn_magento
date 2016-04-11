96dd180ab227bd560ac556ee2cd5f146c70ac206
Aqsa Israr <Aqsa Israr>
2016-04-11 14:45:01 +0500

    added admin page

---
+++ app/code/Techlogix/HelloWorld/Controller/Adminhtml/Employee/Index.php
@@ -0,0 +1,13 @@
+<?php
+    namespace Techlogix\HelloWorld\Controller\Adminhtml\Employee;
+
+    class Index extends \Magento\Backend\App\Action{
+        /**
+         * Index Action for Employee 
+         * @return Void
+         * */
+          
+        public function execute(){
+            die("hello Webkul Employee");
+        }
+    }
