a90f007a548d149905a704a39d5dbe89e695fa42
Aqsa Israr <Aqsa Israr>
2016-04-13 19:09:08 +0500

    added ui-component admin

---
+++ app/code/Aqsa/Recipebox/Block/Adminhtml/Show.php
@@ -0,0 +1,15 @@
+<?php
+namespace Aqsa\Recipebox\Block\Adminhtml;
+
+class Show extends \Magento\Backend\Block\Widget\Grid\Container
+{
+    protected function _construct()
+    {
+        $this->_controller = 'adminhtml_show';
+        $this->_blockGroup = 'Aqsa_Recipebox';
+        $this->_headerText = __('Manage Recipes');
+        parent::_construct();
+        $this->buttonList->update('add', 'label', __('Add New Recipe'));
+    }
+
+}
